4de67bdf7393b014a9aede9fce5dd1ef89b6b3ea
Nikolay <nikolya_usov@bk.ru>
2021-05-21 15:12:09 +0300

    init: api and view

--- src/view/trip-point-card.js
+++ src/view/trip-point-card.js
@@ -1,51 +1,63 @@
 import Abstract from './abstract.js';
+import dayjs from 'dayjs'
 
-const tripPointTemplate = (tripInfo) => {
+const getDestination = (destination, destinations) => {
+  const index = destinations.findIndex((elem) => elem.name === destination);
+  return destinations[index];
+};
+
+const createOffersItem = (offer) => {
+  return `<li class="event__offer">
+  <span class="event__offer-title">${offer}</span>
+  &plus;&euro;&nbsp;
+  <span class="event__offer-price">${offer.price}</span>
+</li>`
+}
+
+const createOffersTitle = (offers) => {
+  return offers.map(createOffersItem).join('');
+}
+
+const tripPointTemplate = (tripInfo, destinations) => {
   const {
-    "base_price": 1100,
-    "date_from": "2019-07-10T22:55:56.845Z",
-    "date_to": "2019-07-11T11:22:13.375Z",
-    "destination": $Destination$,
-    "id": "0",
-    "is_favorite": false,
-    "offers": [
-      {
-        "title": "Choose meal",
-        "price": 180
-      }, {
-        "title": "Upgrade to comfort class",
-        "price": 50
-      }
-    ],
-    "type": "bus"
+    basePrice,
+    dateFrom,
+    dateTo,
+    destination,
+    id,
+    isFavorite,
+    offers,
+    type,
   } = tripInfo;
-  return `<li class="trip-events__item">
+
+  const destinationInfo = getDestination(destination, destinations);
+  const duration = dateTo.diff(dateFrom, 'hour');
+
+  return `<li class="trip-events__item" data-id = "${id}">
   <div class="event">
-    <time class="event__date" datetime="2019-03-18">MAR 18</time>
+    <time class="event__date" datetime="${dayjs(dateFrom).format('YYYY-MM-DD')}">${dayjs(dateFrom).format('МMM DD')}</time>
     <div class="event__type">
       <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
     </div>
-    <h3 class="event__title">Taxi Amsterdam</h3>
+    <h3 class="event__title">${type}${destination}</h3>
     <div class="event__schedule">
       <p class="event__time">
-        <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+        <time class="event__start-time"
+        datetime="${dayjs(dateFrom).format('YYYY-MM-DDTHH:mm')}">${dayjs(dateFrom).format('H:mm')}</time>
         &mdash;
-        <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+        <time class="event__end-time" datetime="${dayjs(dateTo).format('YYYY-MM-DDTHH:mm')}">11:00${dayjs(dateFrom).format('H:mm')}</time>
       </p>
-      <p class="event__duration">30M</p>
+      <p class="event__duration">${duration}</p>
     </div>
     <p class="event__price">
-      &euro;&nbsp;<span class="event__price-value">20</span>
+      &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
     </p>
     <h4 class="visually-hidden">Offers:</h4>
     <ul class="event__selected-offers">
-      <li class="event__offer">
-        <span class="event__offer-title">Order Uber</span>
-        &plus;&euro;&nbsp;
-        <span class="event__offer-price">20</span>
-      </li>
+      ${createOffersTitle(offers)}
     </ul>
-    <button class="event__favorite-btn event__favorite-btn--active" type="button">
+    <button class="event__favorite-btn
+    ${isFavorite ? 'event__favorite-btn--active' : ''}" type="button">
       <span class="visually-hidden">Add to favorite</span>
       <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
         <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -55,5 +67,5 @@ const tripPointTemplate = (tripInfo) => {
       <span class="visually-hidden">Open event</span>
     </button>
   </div>
-</li>`
-}
+</li>`;
+};
